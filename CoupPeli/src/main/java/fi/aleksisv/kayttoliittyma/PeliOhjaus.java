1a6f434060843587dec016d38581a1b8738fe4cf
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2016-09-22 20:11:28 +0300

    Muokkauksia luokkiin.

--- CoupPeli/src/main/java/fi/aleksisv/kayttoliittyma/PeliOhjaus.java
+++ CoupPeli/src/main/java/fi/aleksisv/kayttoliittyma/PeliOhjaus.java
@@ -1,16 +1,18 @@
 package fi.aleksisv.kayttoliittyma;
+
 import fi.aleksisv.logiikka.*;
 import java.util.Random;
 import java.util.Scanner;
+
 /**
  *
  * @author aleksisvuoksenmaa
  */
 public class PeliOhjaus {
+
     private Scanner lukija;
     private Random r;
     Peli peli;
-    
 
     public PeliOhjaus() {
         alkutoimet();
@@ -23,79 +25,105 @@ public class PeliOhjaus {
         System.out.println("Tämä on Coup-Peli. \n"
                 + "------------\n"
                 + "Monellako pelaajalla haluat pelata peliä?");
-        
+
         int i = Integer.parseInt(lukija.nextLine());
         luoPeli(i);
-        
+
     }
-    
+
     private void luoPeli(int pelaajaMaara) {
         this.peli = new Peli(pelaajaMaara);
         peli.luoOsanottajat();
     }
-    
+
     private void pelaa() {
         while (this.peli.getOsanottajajoukko().size() > 1) {
-            int i = siirtovalinta(this.peli.getOsanottajajoukko().get(this.peli.getVuoronumero() % this.peli.getOsanottajajoukko().size()));
-            ilmoitaSiirtovalinnasta(this.peli.getOsanottajajoukko().get(this.peli.getVuoronumero() % this.peli.getOsanottajajoukko().size()), i);
+            siirtovalinta(this.peli.getOsanottajajoukko().get(this.peli.getVuoronumero() % this.peli.getOsanottajajoukko().size()));
+            ilmoitaSiirtovalinnasta(this.peli.getOsanottajajoukko().get(this.peli.getVuoronumero() % this.peli.getOsanottajajoukko().size()), 1);
             this.peli.setVuoronumero(this.peli.getVuoronumero() + 1);
         }
     }
-    
-    private int siirtovalinta (Osanottaja o) {
+
+    private void siirtovalinta(Osanottaja o) {
         if (o instanceof Pelaaja) {
-            return pelaajanSiirtovalinta((Pelaaja) o);
+            pelaajanSiirtovalinta((Pelaaja) o);
         } else if (o instanceof Vastustaja) {
-            return vastustajanSiirtovalinta((Vastustaja) o);
-        } return 1;
+            vastustajanSiirtovalinta((Vastustaja) o);
+        }
     }
-    
-    private int pelaajanSiirtovalinta(Pelaaja pelaaja) {
+
+    private void pelaajanSiirtovalinta(Pelaaja pelaaja) {
         System.out.println("Vuoro numero " + this.peli.getVuoronumero() + ". \n");
         System.out.println("Minkä siirron haluat tehdä?");
         System.out.println("Vaihtoehdot: (1) Income (ota 1 kolikko pankista)\n"
-                    + "             (2) Foreign Aid (ota kaksi kolikkoa pankista)\n"
-                    + "             (3) Coup (maksaa seitsemän kolikkoa, tapa mikä tahansa kortti)\n"
-                    + "             (4) Taxes (ota kolme kolikkoa pankista)\n"
-                    + "             (5) Assassinate (maksa kolme kolikkoa, tapa mikä tahansa kortti)\n"
-                    + "             (6) Steal (ota kaksi kolikkoa toiselta pelaajalta)\n"
-                    + "             (7) Swap Influence (nosta kaksi korttia pakasta ja vaihda yksi omaan pakkaasi).");
-        
-        while(true) {
+                + "             (2) Foreign Aid (ota kaksi kolikkoa pankista)\n"
+                + "             (3) Coup (maksaa seitsemän kolikkoa, tapa mikä tahansa kortti)\n"
+                + "             (4) Taxes (ota kolme kolikkoa pankista)\n"
+                + "             (5) Assassinate (maksa kolme kolikkoa, tapa mikä tahansa kortti)\n"
+                + "             (6) Steal (ota kaksi kolikkoa toiselta pelaajalta)\n"
+                + "             (7) Swap Influence (nosta kaksi korttia pakasta ja vaihda yksi omaan pakkaasi).");
+
+        while (true) {
             int i = Integer.parseInt(lukija.nextLine());
-            if(0 <=i && i <= 7) {
+            if (1 <= i && i <= 7) {
                 System.out.println("Valitsit vaihtoehdon " + i + ".\n");
-                return i;
+                if (i == 1) {
+                    pelaaja.kaytaBasicIncome(this.peli.getPankki());
+                } else if (i == 2) {
+                    pelaaja.kaytaForeignAid(this.peli.getPankki());
+                } else if (i == 3) {
+                    if (pelaaja.getRaha() < 7) {
+                        System.out.println("Sinulla ei ole rahaa tähän siirtoon. Valitse joku toinen. \n");
+                    }
+                    System.out.println("Kenet haluat Coupata?");
+                    int coupattava = Integer.parseInt(lukija.nextLine());
+                    pelaaja.kaytaCoup(this.peli.getPankki(), this.peli.getOsanottajajoukko().get(coupattava));
+                } else if (i == 4) {
+                    pelaaja.kaytaTaxes(this.peli.getPankki());
+                } else if (i == 5) {
+                    if (pelaaja.getRaha() < 3) {
+                        System.out.println("Sinulla ei ole rahaa tähän siirtoon. Valitse joku toinen. \n");
+                    }
+                    System.out.println("Kenet haluat Assassinoida");
+                    int assassinoitava = Integer.parseInt(lukija.nextLine());
+                    pelaaja.kaytaAssassinate(this.peli.getPankki(), this.peli.getOsanottajajoukko().get(assassinoitava));
+                } else if (i == 6) {
+                    System.out.println("Keneltä haluat varastaa?");
+                    int varkaudenKohde = Integer.parseInt(lukija.nextLine());
+                    pelaaja.kaytaSteal(this.peli.getPankki(), this.peli.getOsanottajajoukko().get(varkaudenKohde));
+                } else if (i == 7) {
+                    System.out.println("Toiminnallisuutta ei vielä tueta.");
+                }
             }
             System.out.println("Anna sääntöjen mukainen vastaus. \n");
         }
     }
-    
+
     private int vastustajanSiirtovalinta(Vastustaja vastustaja) {
         System.out.println("Vastustajan " + vastustaja + " vuoro.");
         return r.nextInt(7) + 1;
     }
-    
+
     private void ilmoitaSiirtovalinnasta(Osanottaja osanottaja, int siirtoVaihtoehto) {
         System.out.println("Osanottaja " + osanottaja + " haluaa yrittää siirtoa " + siirtoVaihtoehto + ".");
         boolean pelaajanEpaily = false;
         boolean pelaajanBlokkaus = true;
         Vastustaja vastustajaJokaEpailee = this.peli.kukaVastustajiastaEpailee(osanottaja, siirtoVaihtoehto);
         boolean vastustajanBlokki = this.peli.haluaakoJokuVastustajiastaBlokata(osanottaja, siirtoVaihtoehto);
-        if(!osanottaja.equals(this.peli.getPelaaja())) {
+        if (!osanottaja.equals(this.peli.getPelaaja())) {
             System.out.println("\nHaluatko epäillä siirtoa?");
             pelaajanEpaily = Boolean.parseBoolean(lukija.nextLine());
             System.out.println("\nHaluatko blokata siirron?");
             pelaajanBlokkaus = Boolean.parseBoolean(lukija.nextLine());
         }
-        
-        if(vastustajanBlokki) {
+
+        if (vastustajanBlokki) {
             return;
         } else if (pelaajanBlokkaus) {
             return;
         } else if (pelaajanEpaily) {
             this.peli.getPelaaja().epaileSiirtoa(osanottaja, siirtoVaihtoehto);
-        } else if (!(vastustajaJokaEpailee ==null)) {
+        } else if (!(vastustajaJokaEpailee == null)) {
             vastustajaJokaEpailee.epaileSiirtoa(osanottaja, siirtoVaihtoehto);
         } else {
             suoritaSiirto(osanottaja, siirtoVaihtoehto);
@@ -111,11 +139,11 @@ public class PeliOhjaus {
     }
 
     private void suoritaPelaajanSiirto(int siirtoVaihtoehto) {
-        
+
     }
 
     private void suoritaVastustajanSiirto(int siirtoVaihtoehto) {
-        
+
     }
-    
+
 }
--- CoupPeli/src/test/java/fi/aleksisv/logiikka/PelaajaTest.java
+++ CoupPeli/src/test/java/fi/aleksisv/logiikka/PelaajaTest.java
@@ -29,19 +29,19 @@ public class PelaajaTest {
         Pelaaja pelaaja = new Pelaaja("Pelaaja");
         assertEquals(pelaaja.getNimi(), "Pelaaja");
     }
-    
+
     @Test
     public void getKorttikasiToimii1() {
         Pelaaja pelaaja = new Pelaaja("Pelaaja");
         assertEquals(pelaaja.getKorttikasi().getClass(), Korttikasi.class);
     }
-    
+
     @Test
     public void pelaajallaAlussaOikeaMaaraKortteja() {
         Pelaaja pelaaja = new Pelaaja("Pelaaja");
         assertEquals(pelaaja.getKorttikasi().koko(), 0);
     }
-    
+
     @Test
     public void korttienTappaminenToimii1() {
         Pelaaja pelaaja = new Pelaaja("Pelaaja");
@@ -49,7 +49,7 @@ public class PelaajaTest {
         pelaaja.tapaKortti();
         assertEquals(pelaaja.getKorttikasi().koko(), 1);
     }
-    
+
     @Test
     public void korttienPaljastaminenToimii() {
         Pelaaja pelaaja = new Pelaaja("Pelaaja");
@@ -57,7 +57,7 @@ public class PelaajaTest {
         pelaaja.tapaKortti();
         assertEquals(pelaaja.getKorttikasi().paljastettujenKorttienLukumaara(), 1);
     }
-    
+
     @Test
     public void epailyToimii1() {
         Pelaaja pelaaja1 = new Pelaaja("Aino");
@@ -67,7 +67,7 @@ public class PelaajaTest {
         boolean epaily = pelaaja1.epaile(pelaaja2, new Kortti("Contessa"));
         assertEquals(epaily, false);
     }
-    
+
     @Test
     public void epailyToimii2() {
         Pelaaja pelaaja1 = new Pelaaja("Aino");
@@ -77,7 +77,7 @@ public class PelaajaTest {
         boolean epaily = pelaaja1.epaile(pelaaja2, new Kortti("Contessa"));
         assertEquals(epaily, true);
     }
-    
+
     @Test
     public void epailyToimii3() {
         Pelaaja pelaaja1 = new Pelaaja("Aino");
@@ -87,7 +87,7 @@ public class PelaajaTest {
         boolean epaily = pelaaja1.epaile(pelaaja2, new Kortti("Contessa"));
         assertEquals(epaily, false);
     }
-    
+
     @Test
     public void epailyToimii4() {
         Pelaaja pelaaja1 = new Pelaaja("Aino");
@@ -97,6 +97,153 @@ public class PelaajaTest {
         boolean epaily = pelaaja1.epaile(pelaaja2, new Kortti("Contessa"));
         assertEquals(epaily, true);
     }
+
+    @Test
+    public void basicIncomeToimii1() {
+        Pelaaja p1 = new Pelaaja("Aino");
+        Pankki pankki = new Pankki();
+        p1.kaytaBasicIncome(pankki);
+        assertEquals(p1.getRaha(), 3);
+        assertEquals(pankki.getRahamaara(), 99);
+    }
+
+    @Test
+    public void naytaNakyvatKortitToimii1() {
+        Pelaaja p1 = new Pelaaja("Aino");
+        p1.lisaaKorttiKorttipakkaan(new Kortti("Contessa"));
+        p1.lisaaKorttiKorttipakkaan(new Kortti("Assassin"));
+        p1.tapaKortti();
+        assertEquals(p1.naytaNakyvatKortit(), "Contessa");
+        p1.tapaKortti();
+        assertEquals(p1.naytaNakyvatKortit(), "Contessa, Assassin");
+        p1.tapaKortti();
+        assertEquals(p1.naytaNakyvatKortit(), "Contessa, Assassin");
+    }
+
+    @Test
+    public void saaRahaaToimii1() {
+        Pelaaja p1 = new Pelaaja("Aino");
+        p1.saaRahaa(2);
+        assertEquals(p1.getRaha(), 4);
+    }
+
+    @Test
+    public void menetaRahaaToimii1() {
+        Pelaaja p1 = new Pelaaja("Aino");
+        p1.menetaRahaa(2);
+        assertEquals(p1.getRaha(), 0);
+        p1.menetaRahaa(1);
+        assertEquals(p1.getRaha(), 0);
+        p1.saaRahaa(5);
+        p1.menetaRahaa(6);
+        assertEquals(p1.getRaha(), 0);
+    }
+
+    @Test
+    public void blokkaaToimii1() {
+        Osanottaja o1 = new Osanottaja("Azra");
+        Osanottaja o2 = new Osanottaja("Hmm");
+        o2.lisaaKorttiKorttipakkaan(new Kortti("Contessa"));
+        assertEquals(o1.blokkaa(o2, new Kortti("Assassin")), true);
+        assertEquals(o1.blokkaa(o2, new Kortti("Contessa")), false);
+    }
+
+    @Test
+    public void toStringToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        assertEquals(o.toString(), "Azra");
+    }
+
+    @Test
+    public void annaRahaaPankilleToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        Pankki pankki = new Pankki();
+        int pankinAlkurahat = pankki.getRahamaara();
+        o.annaRahaaPankille(1, pankki);
+        assertEquals(o.getRaha(), 1);
+        assertEquals(pankki.getRahamaara(), 1 + pankinAlkurahat);
+    }
+
+    @Test
+    public void otaRahaaPankiltaToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        Pankki pankki = new Pankki();
+        assertEquals(o.getRaha(), 2);
+        o.otaRahaaPankilta(4, pankki);
+        assertEquals(o.getRaha(), 6);
+    }
+
+    @Test
+    public void kaytaBasicIncomeToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        Pankki pankki = new Pankki();
+        o.kaytaBasicIncome(pankki);
+        assertEquals(o.getRaha(), 3);
+        o.kaytaBasicIncome(pankki);
+        assertNotEquals(o.getRaha(), 3);
+    }
+
+    @Test
+    public void kaytaForeignAidToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        Pankki pankki = new Pankki();
+        o.kaytaForeignAid(pankki);
+        assertEquals(o.getRaha(), 4);
+        o.kaytaForeignAid(pankki);
+        assertNotEquals(o.getRaha(), 4);
+    }
+
+    @Test
+    public void kaytaAssassinateToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        o.saaRahaa(10);
+        Pankki pankki = new Pankki();
+        Osanottaja kohde = new Osanottaja("OiEi");
+        kohde.lisaaKorttiKorttipakkaan(new Kortti("Contessa"));
+        assertEquals(kohde.montakoNakyvaaKorttia(), 0);
+        o.kaytaAssassinate(pankki, kohde);
+        assertEquals(kohde.montakoNakyvaaKorttia(), 1);
+        assertEquals(o.getRaha(), 9);
+        assertEquals(pankki.getRahamaara(), 103);
+    }
+
+    @Test
+    public void kaytaCoupToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        o.saaRahaa(10);
+        Pankki pankki = new Pankki();
+        Osanottaja kohde = new Osanottaja("OiEi");
+        kohde.lisaaKorttiKorttipakkaan(new Kortti("Contessa"));
+        assertEquals(kohde.montakoNakyvaaKorttia(), 0);
+        o.kaytaCoup(pankki, kohde);
+        assertEquals(kohde.montakoNakyvaaKorttia(), 1);
+        assertEquals(o.getRaha(), 5);
+        assertEquals(pankki.getRahamaara(), 107);
+    }
+
+    @Test
+    public void kaytaTaxesToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        Pankki pankki = new Pankki();
+        o.kaytaTaxes(pankki);
+        assertEquals(o.getRaha(), 5);
+        assertEquals(pankki.getRahamaara(), 97);
+    }
+
+    @Test
+    public void montakoNakyvaaKorttiaToimii1() {
+        Osanottaja o = new Osanottaja("Azra");
+        o.lisaaKorttiKorttipakkaan(new Kortti("Contessa"));
+        o.lisaaKorttiKorttipakkaan(new Kortti("Duke"));
+        assertEquals(o.montakoNakyvaaKorttia(), 0);
+        o.tapaKortti();
+        assertEquals(o.montakoNakyvaaKorttia(), 1);
+        o.tapaKortti();
+        assertEquals(o.montakoNakyvaaKorttia(), 2);
+        o.tapaKortti();
+        assertEquals(o.montakoNakyvaaKorttia(), 2);
+    }
+
     @BeforeClass
     public static void setUpClass() {
     }
